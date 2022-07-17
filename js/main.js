85d7bdab1217a988455ba975000f206333950e24
AdrianBTudela <109479685+AdrianBTudela@users.noreply.github.com>
2022-07-17 20:27:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+document.getElementById("div1").appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+camera.position.z = 5;
+
+const mtlLoader = new THREE.MTLLoader();
+mtlLoader.load("assets/pc.mtl", mtlParseResult => {
+    const materials = MtlObjBridge.addMaterialsFromMtlLoader(mtlParseResult);
+    const objLoader = new THREE.OBJLoader();
+    objLoader.addMaterials(materials);
+    objLoader.load("assets/pc.obj", obj => scene.add(obj));
+});
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	renderer.render(scene, camera);
+};
+
+animate();
+        
